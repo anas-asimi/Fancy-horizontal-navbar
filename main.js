29154e54259de4de11d852e8835549f7c5e4b534
anas0bito <96184837+anas0bito@users.noreply.github.com>
2022-05-11 00:53:44 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-var open_button = document.getElementById('open-link') ;
-open_button.addEventListener("click", function () { open_menu() }) ;
+//
+var open_button = document.getElementById("open-link");
+open_button.addEventListener("click", function () {
+  open_menu();
+});
 
-var close_button = document.getElementById('close-link') ;
-close_button.addEventListener("click", function () { close_menu() }) ;
+var close_button = document.getElementById("close-link");
+close_button.addEventListener("click", function () {
+  close_menu();
+});
 
 function open_menu() {
-    document.getElementById('nav-items').style.transform= 'translateY(0px)';
+  document.getElementById("nav-items").style.transform = "translateY(0px)";
+  links.forEach(function (link) {
+      link.style.cssText  = "opacity: 1 ; transition: opacity 0.8s; transition-delay: 0.5s;" ;
+  });
 }
-
 function close_menu() {
-    document.getElementById('nav-items').style.transform= 'translateY(-100vh)';
+  document.getElementById("nav-items").style.transform = "translateY(-100vh)";
+  links.forEach(function (link) {
+    link.style.cssText  = "opacity: 0 " ;
+});
+}
+//
+var links = document.querySelectorAll(".nav-item");
+
+links.forEach(function (link) {
+  link.addEventListener("click", function () {
+    remove_active();
+    link.classList.add("active-link");
+  });
+});
+
+function remove_active() {
+  for (var a = 0; a < links.length; a++) {
+    links[a].classList.remove("active-link");
+  }
 }
